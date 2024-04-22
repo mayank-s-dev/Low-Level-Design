3a5dcb2ac460b51a2b9ed4ddd2569fe1722be82a
Mayank Sahu <mayanksahu.dev@gmail.com>
2024-04-22 10:01:23 +0530

    Fixing liskov

--- src/LiskovSubsitutionPrinciple/Main.java
+++ src/LiskovSubsitutionPrinciple/Main.java
@@ -3,14 +3,14 @@ package LiskovSubsitutionPrinciple;
 import java.util.ArrayList;
 import java.util.List;
 
-// testing liskov
+// testing liskov after fixing
 public class Main {
     public static void main(String args[]){
         List<Vehicle> vehicleList = new ArrayList<>();
         vehicleList.add(new Motorcycle());
         vehicleList.add(new Car());
-//        vehicleList.add(new Bicycle1()); ## this will return null pointer exception during runtime
         for(Vehicle vehicle: vehicleList){
+//        vehicleList.add(new Bicycle1()); ## this will return null pointer exception during runtime
             System.out.println(vehicle.hasEngine().toString());
         }
 
